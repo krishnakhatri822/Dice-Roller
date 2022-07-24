f8613abd3e18ca98c4c923ad5c0e8f5d8514ecbd
Krishna Mahendrabhai Khatri <khatrikrishna822@gmail.com>
2022-07-24 17:49:15 +0530

    Add initial files via upload to Dice-Roller Site

---
+++ myscripts.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1 - 6
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage1);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1 - 6
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImage2);
+
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML= "Player 1 Wins 🚩";
+}
+else if (randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML= "It's a Draw 🏳️";
+}
+else{
+  document.querySelector("h1").innerHTML= "Player 2 Wins 🚩";
+}
